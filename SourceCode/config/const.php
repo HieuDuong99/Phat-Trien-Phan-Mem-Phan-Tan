611c084c9c504b8d9ff05c39d7b7158e534c6e7c
TienAnh0802 <62414498+TienAnh0802@users.noreply.github.com>
2021-05-31 08:18:45 +0700

    Add files via upload

---
+++ SourceCode/config/const.php
@@ -0,0 +1,7 @@
+<?php 
+	define('ROOT', dirname(dirname(__FILE__)));
+	define('STATIC_DIR', '/knn/static');
+	define('STATIC_IMG', STATIC_DIR.'/images');
+	define('UPLOAD_DIR', ROOT.'/static/uploads');
+	define('UPLOAD_IMG', UPLOAD_DIR.'/images');
+ ?>
