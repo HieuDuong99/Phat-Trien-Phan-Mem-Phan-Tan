dcd6d4a212c6e296c26be5d3f27527c87d2624e6
TienAnh0802 <62414498+TienAnh0802@users.noreply.github.com>
2021-05-31 08:16:15 +0700

    Add files via upload

---
+++ SourceCode/base/ModelTemplateController.php
@@ -0,0 +1,9 @@
+<?php 
+require_once ROOT.'/base/Controller.php';
+require_once ROOT.'/base/trait/ModelTemplateTrait.php';
+
+class ModelTemplateController extends Controller {
+	use ModelTemplateTrait;
+}
+
+ ?>
---
+++ SourceCode/base/TemplateController.php
@@ -0,0 +1,9 @@
+<?php 
+require_once ROOT.'/base/Controller.php';
+require_once ROOT.'/base/trait/TemplateTrait.php';
+
+class TemplateController extends Controller {
+	use TemplateTrait;
+}
+
+ ?>
